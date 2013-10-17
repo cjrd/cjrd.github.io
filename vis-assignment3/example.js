6e0c00f949313db29133ef66de1e621a287cf491
Colorado Reed <colorado.j.reed@gmail.com>
2013-10-16 19:15:23 -0700

    fixed auto display problems

--- vis-assignment3/example.js
+++ vis-assignment3/example.js
@@ -5,10 +5,22 @@
  * 15 Oct 2013
  */
 
+  /* TODO global refactorization using OOP */
+  
 (function(d3, topojson){
+  
+  //
+  /*******************
+   *     Setup       *
+   *******************/
+  //
 
+  /*
+   * Consts and "global" variables
+   */
   var viewConsts = {
     summaryClass: "hover-summary",
+    selectedClass: "selected",
     summaryWidth: 350,
     SQRT2DIV2: Math.sqrt(2)/2,
     nodeR: 2.7,
@@ -25,13 +37,128 @@
     }
   };
 
-  // for window resizing
+  var KEYCODES = {UP: 38, DOWN: 40, ENTER: 13},
+      selNum = 0;
+  
+  /* data properties of the input data */
+  var cols = ["absent-data", "clean", "little-health", "medium-health", "severe-health"],
+      dataProps = {"vtemp": "improper holding temperature",
+                   "vcontam": "contaminated equiptment",
+                   "vcook": "inadequate cooking",
+                   "vhygiene": "personal hygiene",
+                   "vsource": "unsafe food source"},
+      dpLen = dataProps.length;
+
+  // variables for window resizing
   var docEl = document.documentElement,
       bodyEl = document.getElementsByTagName('body')[0],
       width = 500,
       height = 500;
+  // set height and width
+  updateWindow();
   
-  // handle window resizing
+  /* keep track of the visible nodes */
+  var circles,
+      insetCircles,
+      circleData;
+
+  /* Setup the main map */
+  var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height),
+      svgG = svg.append("g"),
+      tiler = d3.geo.tile()
+        .size([width, height]),
+      projection = d3.geo.mercator()
+        .center([-122.290, 37.8695])
+        .scale((1 << 21) / Math.PI)
+        .translate([width / 2, height / 2]),
+      path = d3.geo.path()
+        .projection(projection);
+
+  /* setup the inset map */
+  var insetState = {
+    zoomLevel: 8,
+    pos: [-122.26858524322512, 37.871566519480986]
+  },
+      insetWidth = 300,
+      insetHeight = 400,
+      svgInset = d3.select("body").append("svg")
+        .attr("id", "inset")
+        .attr("height", insetHeight)
+        .attr("width", insetWidth),
+      svgInsetG = svgInset.append("g"),
+      projInset;
+
+  //
+  /*******************
+   * Main Application*
+   *******************/
+  //
+
+  /* load the main map */
+  svgG.selectAll("g")
+    .data(tiler
+          .scale(projection.scale() * 2 * Math.PI)
+          .translate(projection([0, 0])))
+    .enter()
+    .append("g")
+    .each(function(d) {
+      var g = d3.select(this);
+      // TODO can we obtain the ocean and whatnot?
+      d3.json("http://" + ["a", "b", "c"][(d[0] * 31 + d[1]) % 3]
+              + ".tile.openstreetmap.us/vectiles-highroad/" + d[2]
+              + "/" + d[0] + "/" + d[1] + ".json", function(error, json) {
+                g.selectAll("path")
+                  .data(json.features.sort(function(a, b){ return a.properties.sort_key - b.properties.sort_key; }))
+                  .enter().append("path")
+                  .attr("class", function(d) { return d.properties.kind; })
+                  .attr("d", path);
+              });
+    });
+
+  /* load the data */
+  d3.csv("Data/geo_food_data.csv", function(data){
+    // cache the results
+    circleData = data;
+    // main circles
+    circles = svgG.append("g").selectAll("circle").data(data);
+    addCircleProps(circles, projection, viewConsts.nodeR);
+    redrawInset();
+    // inset circles
+    insetCircles = svgInsetG.append("g").selectAll("circle").data(data);
+    addCircleProps(insetCircles, projInset, viewConsts.nodeRInset);
+  });
+
+
+  // hardcoded place UCB Campus text for orientation
+  svgG.selectAll("text").data([{text: "U.C.B Campus", pos:[-122.26515201568603, 37.87258281927452] }])
+    .enter().append("text")
+    .attr("x", function(d){ return projection(d.pos)[0];})
+    .attr("y", function(d){ return projection(d.pos)[1];})
+    .text(function(d){return d.text;})
+    .attr("fill","#E097E0");
+
+  // listen for mousedown events on the svg (move the inset box)
+  d3.select(document.body).on("keydown", keyDown);
+  svg.on("mouseup", function(){
+    insetState.pos = projection.invert(d3.mouse(this));
+    redrawInset();
+  });
+
+  // listen for key events on the text input
+  d3.select("#user-text").on("keyup", textKeyUp);
+  
+  window.onresize = updateWindow;
+
+
+  //
+  /*******************
+   *    Functions    *
+   *******************/
+  //
+  
+  /* handle window resizing */
   function updateWindow(){
     width = window.innerWidth || docEl.clientWidth || bodyEl.clientWidth;
     height = window.innerHeight|| docEl.clientHeight|| bodyEl.clientHeight;
@@ -39,40 +166,6 @@
       svg.attr("width", width).attr("height", height);
     }
   }
-  
-  updateWindow();
-  
-  var tiler = d3.geo.tile()
-        .size([width, height]);
-
-  var projection = d3.geo.mercator()
-        .center([-122.290, 37.8695])
-        .scale((1 << 21) / Math.PI)
-        .translate([width / 2, height / 2]);
-
-
-  /* initial inset state */
-  var insetState = {
-    zoomLevel: 8,
-    pos: [-122.26858524322512, 37.871566519480986]
-  };
-  var insetWidth = 300,
-      insetHeight = 400;
-
-  var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height),
-      svgG = svg.append("g");
-
-
-  /* inset initializaiton */
-  var svgInset = d3.select("body").append("svg")
-        .attr("id", "inset")
-        .attr("height", insetHeight)
-        .attr("width", insetWidth),
-      svgInsetG = svgInset.append("g"),
-      projInset;
-
 
   /* redraw the svg inset */
   function redrawInset(){
@@ -140,55 +233,6 @@
     }
   } // end of redrawInset
 
-
-  /* Setup the main map */
-  var path = d3.geo.path()
-        .projection(projection);
-
-  svgG.selectAll("g")
-    .data(tiler
-          .scale(projection.scale() * 2 * Math.PI)
-          .translate(projection([0, 0])))
-    .enter().append("g")
-    .each(function(d) {
-      var g = d3.select(this);
-      // TODO can we obtain the ocean and whatnot?
-      d3.json("http://" + ["a", "b", "c"][(d[0] * 31 + d[1]) % 3]
-              + ".tile.openstreetmap.us/vectiles-highroad/" + d[2]
-              + "/" + d[0] + "/" + d[1] + ".json", function(error, json) {
-                g.selectAll("path")
-                  .data(json.features.sort(function(a, b)
-                                           { return a.properties.sort_key - b.properties.sort_key; }))
-                  .enter().append("path")
-                  .attr("class", function(d) { return d.properties.kind; })
-                  .attr("d", path);
-              });
-    });
-
-
-  var cols = ["absent-data", "clean", "little-health", "medium-health", "severe-health"],
-      dataProps = {"vtemp": "improper holding temperature",
-                   "vcontam": "contaminated equiptment",
-                   "vcook": "inadequate cooking",
-                   "vhygiene": "personal hygiene",
-                   "vsource": "unsafe food source"},
-      dpLen = dataProps.length;
-
-  var circles,
-      insetCircles,
-      circleData;
-  d3.csv("Data/geo_food_data.csv", function(data){
-    // cache the results
-    circleData = data;
-    // main circles
-    circles = svgG.append("g").selectAll("circle").data(data);
-    addCircleProps(circles, projection, viewConsts.nodeR);
-    redrawInset();
-    // inset circles
-    insetCircles = svgInsetG.append("g").selectAll("circle").data(data);
-    addCircleProps(insetCircles, projInset, viewConsts.nodeRInset);
-  });
-
   function addCircleProps(circles, proj, nodeR){
     circles.enter().append("circle")
       .attr("cx", function(d){ return proj([d.lng, d.lat])[0]; })
@@ -223,15 +267,8 @@
         return a.hr - b.hr;
       });
   }
-  // helper function to convert: this string -> This String
-  function toCapCase(str){
-    var splitVals = str.split(/\s+/);
-    for (var i in splitVals){
-      splitVals[i] = splitVals[i][0].toUpperCase() + (splitVals[i].length > 1 ? splitVals[i].slice(1).toLowerCase() : "");
-    }
-    return splitVals.join(" ");
-  }
 
+  /* Get the location of the summary box */
   function getSummaryBoxPlacement(nodeRect, placeLeft){
     var leftMultSign = placeLeft ? -1: 1,
         shiftDiff = (1 + leftMultSign*viewConsts.SQRT2DIV2)*nodeRect.width/2 + leftMultSign;
@@ -242,6 +279,7 @@
     };
   }
 
+  /* draw the summary box */
   function showSummary(d, node){
     var div = document.createElement("div"),
         nodeId = node.attr("id");
@@ -279,7 +317,7 @@
   }
 
   /*
-   * does the query text (qText) fuzzily match the baseText?
+   * Checks if the query text (qText) fuzzily matches the baseText?
    */
   function fuzzyMatch(qText, baseText){
     var qlen = qText.length,
@@ -298,15 +336,14 @@
     return match;
   }
 
-  /**
-   * Dyanimic fuzzy name filtering
-   */
-
-  var KEYCODES = {UP: 38, DOWN: 40, ENTER: 13};
   function keyDown(){
     // show text in the corner if not escape
     var inputTxtBox = document.getElementById("user-text"),
         keyCode = d3.event.keyCode;
+    
+    // remove hover summaries
+    d3.selectAll("." + viewConsts.summaryClass).remove();
+    
     if (document.activeElement.id !== inputTxtBox.id){
       inputTxtBox.value = "";
       inputTxtBox.focus();
@@ -316,35 +353,36 @@
     }
   }
 
-  var selNum;
   function textKeyUp(){
     var keyCode = d3.event.keyCode,
         inText,
         d3Ul = d3.select("#results-list"),
         arrowUp = keyCode == KEYCODES.UP,
         arrowDown = keyCode == KEYCODES.DOWN,
-        enterPressed = keyCode == KEYCODES.ENTER;
+        enterPressed = keyCode == KEYCODES.ENTER,
+        exactMatch = false;
     
     if ( arrowUp || arrowDown){
       // move text selection
       var lis = d3Ul.selectAll("li"),
           numLi = lis[0].length;
       if ( numLi > 1){
-        d3Ul.select(".selected").classed("selected", false);
+        d3Ul.select("." + viewConsts.selectedClass).classed(viewConsts.selectedClass, false);
         if (arrowUp){
           selNum = --selNum < 0 ? numLi-1 : selNum;
         } else if (arrowDown){
           selNum = ++selNum >= numLi ? 0 : selNum;
         }
-        d3.select(lis[0][selNum]).classed("selected", true);
+        d3.select(lis[0][selNum]).classed(viewConsts.selectedClass, true);
         d3.event.preventDefault();
         return;
       }
     } else if (enterPressed){
-      inText = d3Ul.select(".selected").text();
-      d3Ul.selectAll(":not(.selected)").text("");
+      inText = d3Ul.select("." + viewConsts.selectedClass).text();
+      d3Ul.selectAll(":not(." + viewConsts.selectedClass + ")").text("");
       this.value = " ";
       this.blur();
+      exactMatch = true;
       // show over info for el
     } else{
       inText = this.value.toLowerCase();
@@ -353,20 +391,27 @@
     var inTextLen = inText.length,
         fmatch,
         nshow = 0,
-        d3this;
+        d3this,
+        movedSquare = false;
     // clear list
     d3Ul.html("");
-    // fill list with top 3 fuzzy matches
+    // fill list with top 3 fuzzy matches unless exactMatch is set
     if (inTextLen > 0){
       circles.each(function(d){
         d3this = d3.select(this);
-        fmatch = fuzzyMatch(inText, d.name);
+        fmatch = exactMatch ? inText === d.name : fuzzyMatch(inText, d.name);
         if (fmatch){
+          if (exactMatch && !movedSquare){
+            insetState.pos = [d.lng, d.lat];
+            redrawInset();
+            movedSquare = true;
+            showSummary(d, d3this);
+          }
           d3this.attr("r", viewConsts.nodeR);
           if (nshow < 20){
             var li = d3Ul.append("li").text(d.name);
             if (nshow == 0){
-              li.classed("selected", true);
+              li.classed(viewConsts.selectedClass, true);
               selNum = 0;
             }
             nshow++;
@@ -389,25 +434,14 @@
     }
   }
 
-  // hardcoded place UCB Campus text for orientation
-  svgG.selectAll("text").data([{text: "U.C.B Campus", pos:[-122.26515201568603, 37.87258281927452] }])
-      .enter().append("text")
-      .attr("x", function(d){ return projection(d.pos)[0];})
-      .attr("y", function(d){ return projection(d.pos)[1];})
-      .text(function(d){return d.text;})
-      .attr("fill","#E097E0");
-
-  // listen for mousedown events on the svg (move the inset box)
-  d3.select(document.body).on("keydown", keyDown);
-  svg.on("mouseup", function(){
-    insetState.pos = projection.invert(d3.mouse(this));
-    redrawInset();
-  });
-
-  // listen for key events on the text input
-  d3.select("#user-text").on("keyup", textKeyUp);
-  
-  window.onresize = updateWindow;
+  // helper function to convert: this string -> This String
+  function toCapCase(str){
+    var splitVals = str.split(/\s+/);
+    for (var i in splitVals){
+      splitVals[i] = splitVals[i][0].toUpperCase() + (splitVals[i].length > 1 ? splitVals[i].slice(1).toLowerCase() : "");
+    }
+    return splitVals.join(" ");
+  }
 })(window.d3, window.topojson);
 
 
